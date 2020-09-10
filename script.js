29a90af869259e769a7df75df4222c1378c4e3f7
cgriffin332 <cgriffin332@gmail.com>
2020-09-10 16:10:44 -0400

    added current date and time that is live on top of page.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+/**
+ * DOM Variables
+ */
+var date = $("#currentDay");
+
+
+$(document).ready(function(){
+
+// display current date and time on scheduler
+var update = function(){
+    date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}
+// update every second
+setInterval(update, 1000);
+
+
+})
