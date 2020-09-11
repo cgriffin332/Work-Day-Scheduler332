c6c609705125a7601fd2368db631596fa2f55ed3
cgriffin332 <cgriffin332@gmail.com>
2020-09-11 16:48:31 -0400

    ran prettier

--- script.js
+++ script.js
@@ -20,97 +20,101 @@ var input2pm = $("#14");
 var input3pm = $("#15");
 var input4pm = $("#16");
 var input5pm = $("#17");
-var inputAll = $(".text")
+var inputAll = $(".text");
 
 /**
  * Variables
  */
 // an array of inputs
-var inputs = [input9am, input10am, input11am, input12pm, input1pm, input2pm, input3pm, input4pm, input5pm]
+var inputs = [
+  input9am,
+  input10am,
+  input11am,
+  input12pm,
+  input1pm,
+  input2pm,
+  input3pm,
+  input4pm,
+  input5pm,
+];
 // current date and time
 var objDate = new Date();
 var hours = objDate.getHours();
 
-
-
-
-var populateSchedule = function(){
-    for (var i = 0; i < inputAll.length; i++) {
+var populateSchedule = function () {
+  for (var i = 0; i < inputAll.length; i++) {
     inputs[i].text(JSON.parse(localStorage.getItem(inputs[i][0].id)));
-    }
-}
+  }
+};
 
 populateSchedule();
 
 //This all begins when document is ready
-$(document).ready(function(){
-    // function to update the class of timeslot based on current local time
-    var updateSlots = function(){
-        for (var i = 0; i < inputAll.length; i++) {
-            // if the time slot is the same as curent time, class is present
-            if (parseInt(inputs[i][0].id) === hours) {
-                inputs[i].addClass("present");
-            }
-            // clas of past is assigned if its in the past
-            else if (parseInt(inputs[i][0].id) < hours) {
-                inputs[i].addClass("past");
-            }
-            // otherwise the class of future is assigned
-            else {
-                inputs[i].addClass("future");
-            }
-        }
-
+$(document).ready(function () {
+  // function to update the class of timeslot based on current local time
+  var updateSlots = function () {
+    for (var i = 0; i < inputAll.length; i++) {
+      // if the time slot is the same as curent time, class is present
+      if (parseInt(inputs[i][0].id) === hours) {
+        inputs[i].addClass("present");
+      }
+      // clas of past is assigned if its in the past
+      else if (parseInt(inputs[i][0].id) < hours) {
+        inputs[i].addClass("past");
+      }
+      // otherwise the class of future is assigned
+      else {
+        inputs[i].addClass("future");
+      }
     }
-    //call this updateSlot function
-    updateSlots();
-// display current date and day on scheduler
-var update = function(){
-    date.text(moment().format('dddd, MMMM Do'));
-}
+  };
+  //call this updateSlot function
+  updateSlots();
+  // display current date and day on scheduler
+  var update = function () {
+    date.text(moment().format("dddd, MMMM Do"));
+  };
 
-// update time every second
-setInterval(update, 1000);
-// update time slots every 10 seconds
-setInterval(updateSlots, 10000)
+  // update time every second
+  setInterval(update, 1000);
+  // update time slots every 10 seconds
+  setInterval(updateSlots, 10000);
 
-// when user clicks a save button, it saves value to local storage
-save9am.on("click", function(event){
+  // when user clicks a save button, it saves value to local storage
+  save9am.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("9", JSON.stringify($(input9am).val()));
-})
-save10am.on("click", function(event){
+  });
+  save10am.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("10", JSON.stringify($(input10am).val()));
-})
-save11am.on("click", function(event){
+  });
+  save11am.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("11", JSON.stringify($(input11am).val()));
-})
-save12pm.on("click", function(event){
+  });
+  save12pm.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("12", JSON.stringify($(input12pm).val()));
-})
-save1pm.on("click", function(event){
+  });
+  save1pm.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("13", JSON.stringify($(input1pm).val()));
-})
-save2pm.on("click", function(event){
+  });
+  save2pm.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("14", JSON.stringify($(input2pm).val()));
-})
-save3pm.on("click", function(event){
+  });
+  save3pm.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("15", JSON.stringify($(input3pm).val()));
-})
-save4pm.on("click", function(event){
+  });
+  save4pm.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("16", JSON.stringify($(input4pm).val()));
-})
-save5pm.on("click", function(event){
+  });
+  save5pm.on("click", function (event) {
     event.preventDefault();
     localStorage.setItem("17", JSON.stringify($(input5pm).val()));
-})
-
-
-})
+  });
+});
